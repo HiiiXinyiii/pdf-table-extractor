0c96f260723970f6c2bf6abc62103f6d50e4aefb
Xinyi Chen <xinyichen@Xinyis-MacBook-Pro.local>
2024-06-28 16:49:21 +0800

    use seperate file

--- parse-cmd.js
+++ parse-cmd.js
@@ -1,7 +1,7 @@
 // Ex: node parse-cmd.js samples/pta_10229_131308_94274.pdf
 
 var fs = require('fs');
-Image = function(){ };
+Image = function () { };
 
 // HACK few hacks to let PDF.js be loaded not as a module in global space.
 require('./pdf.js/examples/node/domstubs.js');
@@ -13,16 +13,28 @@ PDFJS.cMapUrl = './pdf.js/build/generic/web/cmaps/';
 PDFJS.cMapPacked = true;
 
 
+// Parse the format
+var parse_format = require("./parse_format.js");
+
+
 // Loading file from file system into typed array
 var pdfPath = process.argv[2];
-var data = new Uint8Array(fs.readFileSync(pdfPath));
+var data = new Uint8Array(fs.readFileSync(pdfPath));        // a Buffer
 
-// Will be using promises to load document, pages and misc data instead of
-// callback.
+
+// Will be using promises to load document, pages and misc data instead of callback.
 PDFJS.getDocument(data).promise.then(pdf_table_extractor).then(function (result) {
-    console.log(JSON.stringify(result));
-    // fs.writeFileSync('output.json', JSON.stringify(result));
+    // Parse the result into right formats
+    var {jsonResult, htmlResult, csvResult} = parse_format.parse_format(result)
+    
+    // Write into files
+    fs.writeFileSync('outputs/output.json', jsonResult);
+    fs.writeFileSync('outputs/output.html', htmlResult);
+    fs.writeFileSync('outputs/output.csv', csvResult);
 }, function (err) {
     console.error('Error: ' + err, err.stack);
 });
+
+
+
 
